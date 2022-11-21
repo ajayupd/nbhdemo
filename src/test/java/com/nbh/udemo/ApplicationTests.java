d94001ae759ec426dc908ddfbb6cc6b1e29b40c0
Upadhyay <aupd@c02f5bgtmd6n.home>
2022-11-21 12:29:57 -0500

    first commit

---
+++ src/test/java/com/nbh/udemo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbh.udemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
